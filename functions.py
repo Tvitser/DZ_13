ed91caadb93dfb324e5ae30546a466c678695c6c
Павел <fotocrysis36@gmail.com>
2022-01-02 13:42:24 +0300

    added post GET and post POST

--- functions.py
+++ functions.py
@@ -15,3 +15,15 @@ def get_tags(data):
             if word.startswith("#"):
                 results.add(word[1:])
     return results
+
+def get_posts_by_tag(data, tag):
+    results=[]
+    for record in data:
+        if f'#{tag}' in record["content"]:
+            results.append(record)
+    return results
+def add_post(filename, post):
+    data=read_json(filename)
+    data.append(post)
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4, sort_keys=True)
