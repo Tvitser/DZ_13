9694ecc3ffd8d579266ad47a571d78a771ec9a16
Павел <fotocrysis36@gmail.com>
2022-01-02 12:48:27 +0300

    added main page

--- functions.py
+++ functions.py
@@ -1,1 +1,17 @@
 import json
+
+
+def read_json(file):
+    with open(file, encoding="utf-8") as f:
+        return json.load(f)
+
+
+def get_tags(data):
+    results = set()
+    for i in data:
+        content = i['content']
+        words = content.split()
+        for word in words:
+            if word.startswith("#"):
+                results.add(word[1:])
+    return results
